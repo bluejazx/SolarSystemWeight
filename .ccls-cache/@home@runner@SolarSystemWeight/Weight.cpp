fc6434d2d8a832df8e4c6d0fba2d1efbd6a215d6
bluejazx <108899308+bluejazx@users.noreply.github.com>
2024-05-23 15:07:54 +0000

    Added option for all planets

--- .ccls-cache/@home@runner@SolarSystemWeight/Weight.cpp
+++ .ccls-cache/@home@runner@SolarSystemWeight/Weight.cpp
@@ -20,8 +20,8 @@ int main()
   std::cout << "Enter your weight in pounds below \n";
   std::cin >> weight;
   std::cout << "\nHeres a list of planets:\n";
-  std::cout << " 1.Mercury\n 2.Venus\n 3.Earth\n 4.Mars\n 5.Jupiter\n 6.Saturn\n 7.Uranus\n 8.Neptune \n 9.Pluto\n";
-  std::cout << "\nEnter the number 1-9 planet you want to know your weight on:\n";
+  std::cout << " 1.Mercury\n 2.Venus\n 3.Earth\n 4.Mars\n 5.Jupiter\n 6.Saturn\n 7.Uranus\n 8.Neptune \n 9.Pluto\n 10.All";
+  std::cout << "\nEnter the number 1-10 planet you want to know your weight on:\n";
   std::cin >> planet;
 
   //caculates the weight for each planet in the solar system based on the number entern and diplays thiert weight
@@ -69,6 +69,26 @@ int main()
       break;
     //pluto
     case 9:
+      pluto = weight*.083;
+      std::cout << "Your weight on Pluto is: " << pluto << "lbs \n";
+      break;
+    case 10:
+      mercury = weight*.38;
+      std::cout << "\nYour weight on Mercury is: " << mercury << "lbs \n";
+      venus = weight*.91;
+      std::cout << "Your weight on Venus is: " << venus << "lbs \n";
+      earth = weight;
+      std::cout << "Your weight on Earth is: " << earth << "lbs \n";
+      mars = weight*.38;
+      std::cout << "Your weight on Mars is: " << mars << "lbs \n";
+      jupiter = weight*2.4;
+      std::cout << "Your weight on Jupiter is: " << jupiter << "lbs \n";
+      saturn = weight*1.08;
+      std::cout << "Your weight on Saturn is: " << saturn << "lbs \n";
+      uranus = weight*.86;
+      std::cout << "Your weight on Uranus is: " << uranus << "lbs \n";
+      neptune = weight*1.1;
+      std::cout << "Your weight on Neptune is: " << neptune << "lbs \n";
       pluto = weight*.083;
       std::cout << "Your weight on Pluto is: " << pluto << "lbs \n";
       break;
